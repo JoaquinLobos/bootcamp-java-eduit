28aea086e9ecf0978b026cf0a15bcfb833ab5710
joaquin-Lobos <72621101+joaquin-Lobos@users.noreply.github.com>
2022-03-26 21:51:36 -0300

    - segundo commit

--- app-web/src/main/java/primariaXYZ/ConstructorAlumno.java
+++ app-web/src/main/java/primariaXYZ/ConstructorAlumno.java
@@ -3,8 +3,30 @@ package primariaXYZ;
 
 public class ConstructorAlumno {
 	
-	String nombre = "gerardo";
-	String apellido = "lopez";
-	int edad = 13;
+	String nombre;
+	String apellido;
+	int examenesAlumno;
+	int notasExamenes[];
 	
+	public ConstructorAlumno(String nombre, String apellido, int examenesAlumno, int notasExamenes[]) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.examenesAlumno = examenesAlumno;
+		this.notasExamenes = new int[examenesAlumno];
+		for (int i1 = 0; i1 < examenesAlumno; ++i1) {
+			this.notasExamenes[i1] = notasExamenes[i1];
+		}
+	}
+	
+	public static void Debugger(ConstructorAlumno alumno) {
+		System.out.println("nombre: " + alumno.nombre + " " + alumno.apellido);
+		System.out.println("cantidad de examenes realizados en el anio: " + alumno.examenesAlumno);
+		
+		for (int i1 : alumno.notasExamenes) {
+			int contador = 0;
+			System.out.println("nota " + contador + ": " + i1);
+			contador++;
+		}
+	}
+
 }
--- app-web/src/main/java/primariaXYZ/Main.java
+++ app-web/src/main/java/primariaXYZ/Main.java
@@ -8,22 +8,44 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);	
-		ArrayList Alumnos = new ArrayList();
+		ArrayList<ConstructorAlumno> alumnos = new ArrayList<ConstructorAlumno>();
 		System.out.println("ingresar numero de alumnos a evaluar");
+		ConstructorAlumno alumno = null;
 		int numeroAlumnos = teclado.nextInt();
+		
+		
 					
-			for (int i = 0; i < 5; ++i) {
-				    
-				ConstructorAlumno Alumno = new ConstructorAlumno();
-				Alumnos.add("something");
-					
-					
-				System.out.println(Alumnos.toArray());
-					
-				if (i >= numeroAlumnos) {
+			for (int i = 0; i < numeroAlumnos; ++i) {
+				System.out.println("ingresar nombre del alumno");
+				String nombreAlumno = teclado.next();
+				System.out.println("ingresar apellido del alumno");
+				String apellidoAlumno = teclado.next();
+				System.out.println("ingresar cantidad de examenes que realizó el alumno en el año");
+				int examenesAlumno = teclado.nextInt();
+				
+				if(examenesAlumno > 5) {
+					System.out.println("el alumno no puede hace mas examenes de los evaluados en el año");
 					break;
 				}
+				
+				int notasExamenes[];
+				notasExamenes = new int[examenesAlumno];
+				
+				for (int i1 = 0; i1 < examenesAlumno; ++i1) {
+					System.out.println("ingresar notas");
+					int nota = teclado.nextInt();
+					notasExamenes[i1] = nota;
+				}
+				
+				alumno = new ConstructorAlumno(nombreAlumno, apellidoAlumno, examenesAlumno, notasExamenes);
+				alumnos.add(alumno);	
+				
 			}
+			
+		for (ConstructorAlumno i : alumnos) {
+			ConstructorAlumno.Debugger(i);
+		}
+		
 		teclado.close();
 	}
 }
