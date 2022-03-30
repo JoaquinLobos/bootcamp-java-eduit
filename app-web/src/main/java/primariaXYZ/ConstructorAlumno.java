e5518469a5339394f7761288b723bcf524d610ab
joaquin-Lobos <72621101+joaquin-Lobos@users.noreply.github.com>
2022-03-30 13:02:02 -0300

    - ultimo commit de la tarea semanal 2 - ejercicio realizado listo para correccion

--- app-web/src/main/java/primariaXYZ/ConstructorAlumno.java
+++ app-web/src/main/java/primariaXYZ/ConstructorAlumno.java
@@ -6,34 +6,27 @@ public class ConstructorAlumno {
 	String nombre;
 	String apellido;
 	int examenesAlumno;
-	int notasExamenes[];
+	float notasExamenes[];
+	float promedioAlumno;
 	
-	public ConstructorAlumno(String nombre, String apellido, int examenesAlumno, int notasExamenes[]) {
+	public ConstructorAlumno(String nombre, String apellido, int examenesAlumno, float notasExamenes[]) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.examenesAlumno = examenesAlumno;
-		this.notasExamenes = new int[examenesAlumno];
+		this.notasExamenes = new float[examenesAlumno];
 		for (int i1 = 0; i1 < examenesAlumno; ++i1) {
 			this.notasExamenes[i1] = notasExamenes[i1];
 		}
 	}
 	
-	public static void Debugger(ConstructorAlumno alumno, int nota) {
+	public static void Debugger(ConstructorAlumno alumno, float nota) {
 		
 		System.out.println("nombre: " + alumno.nombre + " " + alumno.apellido);
 		System.out.println("nota: " + nota);
-		
-		
-		
-		
-		
-		
-		//System.out.println("cantidad de examenes realizados en el anio: " + alumno.examenesAlumno);
-		//int contador = 0;
-		//for (int i1 : alumno.notasExamenes) {
-		//  System.out.println("nota " + contador + ": " + i1);
-		//	contador++;	
-		//}
+	}
+	
+	public void Promedio(float promedioAlumno) {
+		this.promedioAlumno = promedioAlumno;
 	}
 
 }
--- app-web/src/main/java/primariaXYZ/Main.java
+++ app-web/src/main/java/primariaXYZ/Main.java
@@ -11,10 +11,13 @@ public class Main {
 		ArrayList<ConstructorAlumno> alumnos = new ArrayList<ConstructorAlumno>();
 		ArrayList<ConstructorAlumno> alumnosNotaMasAlta = new ArrayList<ConstructorAlumno>();
 		ArrayList<ConstructorAlumno> alumnosNotaMasBaja = new ArrayList<ConstructorAlumno>();
-		int notaMasAlta = 0;
-		int notaMasBaja = 10;
+		int examenesAlumno = 0;
+		float notaMasAlta = 0;
+		float notaMasBaja = 10;
 		System.out.println("ingresar numero de alumnos a evaluar");
 		ConstructorAlumno alumno = null;
+		float sumaTotal = 0;
+		float promedioAlumno = 0;
 		int numeroAlumnos = teclado.nextInt();
 		
 		
@@ -26,7 +29,7 @@ public class Main {
 				System.out.println("ingresar apellido del alumno");
 				String apellidoAlumno = teclado.next();
 				System.out.println("ingresar cantidad de examenes que realizó el alumno en el año");
-				int examenesAlumno = teclado.nextInt();
+				examenesAlumno = teclado.nextInt();
 				
 				//verificar que la cantidad de examenes ingresada no sobrepase la cantidad de examenes tomados en el anio
 				if(examenesAlumno > 5) {
@@ -35,30 +38,31 @@ public class Main {
 				}
 				
 				//crear el array para las notas de cada examen
-				int notasExamenes[];
-				notasExamenes = new int[examenesAlumno];
+				float notasExamenes[];
+				notasExamenes = new float[examenesAlumno];
 				
-				// ingresar todas las notas de todos lo examenes
+				// ingresar todas las notas de todos los examenes
 				for (int i1 = 0; i1 < examenesAlumno; ++i1) {
 					System.out.println("ingresar notas");
-					int nota = teclado.nextInt();
+					float nota = teclado.nextFloat();
 					
-					if(nota > 10) {
+					if(nota > 10 || nota < 0) {
+						System.out.println("la nota no puede ser mayor a 10 ni menor que 0");
 						break;
 					}
+					
 					notasExamenes[i1] = nota;
 				}
 				
 				//crear el objeto con todos los datos y guardarlo en el array
 				alumno = new ConstructorAlumno(nombreAlumno, apellidoAlumno, examenesAlumno, notasExamenes);
 				alumnos.add(alumno);	
+			}
 				
 				
-				//verificar que alumno sacó la nota mas alta
-				
-				
+				//verificar la nota mas alta y mas baja de todo el curso
 				for (ConstructorAlumno i1 : alumnos) {
-					for (int i2 : i1.notasExamenes ) {
+					for (float i2 : i1.notasExamenes ) {
 						if (i2 > notaMasAlta) {
 							notaMasAlta = i2;
 						}
@@ -67,10 +71,14 @@ public class Main {
 						}
 					}
 				}
+				
+				//guardar a los alumnos que obtuvieron esas notas
 				for (ConstructorAlumno i1 : alumnos) {
+					//booleanos para que no se guarden alumnos repetidos
+					sumaTotal = 0;
 					boolean verificado1 = false;
 					boolean verificado2 = false;
-					for (int i2 : i1.notasExamenes) {
+					for (float i2 : i1.notasExamenes) {
 						if (notaMasAlta == i2 && verificado1 == false) {
 							alumnosNotaMasAlta.add(i1);
 							verificado1 = true;
@@ -79,27 +87,61 @@ public class Main {
 							alumnosNotaMasBaja.add(i1);
 							verificado2 = true;
 						}
+						sumaTotal += i2;
 					}
+					promedioAlumno = sumaTotal / 5;
+					i1.Promedio(promedioAlumno);
+
+					System.out.println("promedio es igual a: " + sumaTotal + "/ " + examenesAlumno + "=" + promedioAlumno);
 				}
 				
-			}
+				
+				
+			boolean esPromedio = false;
 			String mnsj = "Los alumnos con las mejores notas son:";
-			Debug(alumnosNotaMasAlta, notaMasAlta, mnsj);
+			Debug(alumnosNotaMasAlta, notaMasAlta, mnsj, esPromedio);
 			mnsj = "Los alumnos con las notas mas bajas son:";
-			Debug(alumnosNotaMasBaja, notaMasBaja, mnsj);
+			Debug(alumnosNotaMasBaja, notaMasBaja, mnsj, esPromedio);
+			mnsj = " ";
+			esPromedio = true;
+			Debug(alumnos, 0, mnsj, esPromedio);
+			
 
 		// llamar a la funcion debugger para mostrar por consola todos los datos de cada alumno	
-
 		
 		teclado.close();
 	}
 	
 	
 	
-	public static void Debug(ArrayList<ConstructorAlumno> alumnos, int nota, String m) {
-		System.out.println(m);
-		for (ConstructorAlumno i : alumnos) {
-			ConstructorAlumno.Debugger(i, nota);
+	public static void Debug(ArrayList<ConstructorAlumno> alumnos, float nota, String m, boolean esPromedio) {
+		if (esPromedio) {
+			System.out.println("los alumnos que promocionan son:");
+			System.out.println(" "); // <<< estos printIn estan para que sea mas legible la consola :)
+			for (ConstructorAlumno i : alumnos) {
+				if (i.promedioAlumno >= 7) {
+					nota = i.promedioAlumno;
+					ConstructorAlumno.Debugger(i, nota);
+					System.out.println(" ");
+				}
+			}
+			System.out.println("los alumnos que NO promocionan son");
+			System.out.println(" ");
+			for (ConstructorAlumno i : alumnos) {
+				if (i.promedioAlumno < 7) {
+					nota = i.promedioAlumno;
+					ConstructorAlumno.Debugger(i, nota);
+					System.out.println(" ");
+				}
+			}
+		}
+		else {
+			System.out.println(m);
+			System.out.println(" ");
+			for (ConstructorAlumno i : alumnos) {
+				ConstructorAlumno.Debugger(i, nota);
+				System.out.println(" ");
+			}
 		}
 	}
 }
